b035c41a6573e699199474aefb674900b1d8bc09
azuax <azuaxx@gmail.com>
2024-10-26 00:29:27 -0300

    Updated workflow for deprecated uses

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Lambda CI/CD deployment.')
+        'body': json.dumps('My Lambda CI/CD deployment.')
     }
