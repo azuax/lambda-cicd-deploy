b9151d2df003c41fb57810b3c872db6694230342
azuax <azuaxx@gmail.com>
2024-10-26 00:18:01 -0300

    Deploy

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Lambda CI/CD working')
+        'body': json.dumps('Lambda CI/CD deployment')
     }
